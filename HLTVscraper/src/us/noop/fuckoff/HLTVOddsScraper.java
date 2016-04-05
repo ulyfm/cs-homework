7aed1c16f5c752bfdb75743f2f4be4b79bd9fe6a
714daniel <714.daniel.smith@gmail.com>
2016-04-05 11:39:49 -0700

    Added start of HLTVOdds

---
+++ HLTVscraper/src/us/noop/fuckoff/HLTVOddsScraper.java
@@ -0,0 +1,34 @@
+package us.noop.fuckoff;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class HLTVOddsScraper {
+	private String teamA,teamB;
+	private URL u;
+	public HLTVOddsScraper(String teamA, String teamB, URL u) {
+		this.teamA = teamA;
+		this.teamB = teamB;
+		this.u = u;
+	}
+	public HashMap<String, String> getOdds () {
+		Elements el;
+		try {
+			Document d = Jsoup.connect(u.toString()).userAgent("Chrome").get();
+			el = d.getElementsByAttribute("text-align");
+		} catch (IOException e) {
+			System.err.println("no connection for URL" );
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
+
+}
